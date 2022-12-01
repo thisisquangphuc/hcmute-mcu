b3014afaf2fcf07994669c10f8b8db398c3e902f
Le Quang Phuc <42735626+thisisquangphuc@users.noreply.github.com>
2022-12-01 23:41:07 +0700

    upload code day3

---
+++ 1-ON-TAP-VDK-HK1-22-23/DAY3/cau4_pre.c
@@ -0,0 +1,28 @@
+#include <16f887.h>
+#fuses hs,put,noprotect,nowdt   //intrc_io
+#use delay(clock=10Mhz)
+
+#bit TMR1IF =0x0c.0
+#define PULSE pin_e0
+
+unsigned int8 count=0;
+
+
+void main ()
+{
+   setup_timer_1(T1_INTERNAL|T1_DIV_BY_4);
+   set_timer1(64911);
+   while (true)
+   {
+      if(TMR1IF==1) //Tràn 
+      {
+         //du 1ms
+         set_timer1(64911);  
+         TMR1IF=0;      //xoa co tran
+         count++;
+      }
+      if(count<1)      output_high(PULSE);
+      else if(count<4)  output_low(PULSE);
+      else count=0;
+   }
+}
